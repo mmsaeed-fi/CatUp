381b5c6cceb0b3af7b4596acc658162dacde8fe0
mmsaeed-pexip <mustafa.saeed@pexip.com>
2023-01-13 16:30:12 +0100

    Initial Commit

---
+++ CatUp/CatUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatUpApp.swift
+//  CatUp
+//
+//  Created by Mustafa Saeed on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CatUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
